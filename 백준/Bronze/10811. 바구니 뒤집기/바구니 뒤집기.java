f950cf2980e17b66664ecc03530677c720f7608f
SANG JAE AHN <168457116+ahnGlenn@users.noreply.github.com>
2024-09-19 13:16:20 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 192 ms, Memory: 18572 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -18,6 +18,11 @@ public class Main {
             int a = sc.nextInt(); // arrs[a]부터
             int b = sc.nextInt(); // arrs[b]까지 역순으로 변경할거임
 
+            // a = 1, b= 4
+            //a[0] a[1] a[2] a[3] a[4] a[5]
+            // 0    1    2    3    4   5
+            //          t:
+            
             // 앞뒤로 바꿔줌
             while(a < b){
                 int tmp = basket[a];
@@ -25,7 +30,6 @@ public class Main {
                 basket[b--] = tmp;
             }
         }
-
         for(int i=1; i<=N; i++){
             System.out.print(basket[i] + " " );
         }
