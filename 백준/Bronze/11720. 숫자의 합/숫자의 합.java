fa6a1319e1caf9afe23d08a577ca03d519e878dd
SANG JAE AHN <dkstkdwo93@naver.com>
2025-02-09 23:57:22 +0900

    [Bronze IV] Title: 숫자의 합, Time: 172 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+        for(int i=0; i<sNum.length(); i++){
+            sum += cNum[i] - '0';
+        }
+        System.out.println(sum);
+    }
+}
